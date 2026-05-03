4944d7c81f4f2128a20b626d127585a29c1f99b2
Domas Leščinskas <domas.lescinskas@gmail.com>
2026-05-03 12:36:08 +0300

    Fixed service symlinking

--- iso-profile/genapkovl.sh
+++ iso-profile/genapkovl.sh
@@ -18,13 +18,17 @@ fi
 
 echo "$HOSTNAME" > "$TMP"/etc/hostname
 
+ln -sf /etc/init.d/hwdrivers "$TMP"/etc/runlevels/sysinit/hwdrivers
+ln -sf /etc/init.d/modloop "$TMP"/etc/runlevels/sysinit/modloop
 ln -sf /etc/init.d/devfs "$TMP"/etc/runlevels/sysinit/devfs
 ln -sf /etc/init.d/dmesg "$TMP"/etc/runlevels/sysinit/dmesg
 ln -sf /etc/init.d/mdev "$TMP"/etc/runlevels/sysinit/mdev
 
+ln -sf /etc/init.d/sysctl "$TMP"/etc/runlevels/boot/sysctl
+ln -sf /etc/init.d/syslog "$TMP"/etc/runlevels/boot/syslog
 ln -sf /etc/init.d/hwclock "$TMP"/etc/runlevels/boot/hwclock
 ln -sf /etc/init.d/modules "$TMP"/etc/runlevels/boot/modules
+ln -sf /etc/init.d/bootmisc "$TMP"/etc/runlevels/boot/bootmisc
 ln -sf /etc/init.d/hostname "$TMP"/etc/runlevels/boot/hostname
-ln -sf /etc/init.d/hwdrivers "$TMP"/etc/runlevels/boot/hwdrivers
 
 tar -C "$TMP" -c . | gzip -9 > "$HOSTNAME.apkovl.tar.gz"
