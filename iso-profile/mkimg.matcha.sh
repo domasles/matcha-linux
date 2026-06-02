d296243d3dc3db2f2d51af7a7b748fc0aa7143c7
Domas Leščinskas <domas.lescinskas@gmail.com>
2026-06-02 18:18:17 +0300

    Rewritten Matcha Linux ISO creation profile to resolve dependency conflicts when installing system

--- iso-profile/mkimg.matcha.sh
+++ iso-profile/mkimg.matcha.sh
@@ -1,15 +1,38 @@
 profile_matcha() {
-    profile_standard
+    kernel_flavors="lts"
+    initfs_cmdline="modules=loop,squashfs,sd-mod,usb-storage quiet"
+    initfs_features="ata base bootchart cdrom dhcp ext4 mmc nvme raid scsi squashfs usb virtio nfit"
+    modloop_sign=yes
+    grub_mod="all_video disk part_gpt part_msdos linux normal configfile search search_label efi_gop fat iso9660 cat echo ls test true help gzio multiboot2 efi_uga"
+
+    profile_abbrev="matcha"
+    image_ext="iso"
+    output_format="iso"
+    arch="x86_64"
+    kernel_addons="xtables-addons"
     kernel_cmdline="unionfs_size=512M console=tty0"
     syslinux_serial="0 115200"
     apkovl="genapkovl.sh"
     image_name="matcha-linux"
     title="Matcha Linux"
-    apks="$apks alpine-base git sudo networkmanager
+    hostname="matcha"
+
+    apks="alpine-base git sudo networkmanager
         elogind polkit eudev udev-init-scripts
         udev-init-scripts-openrc dbus gnome gnome-apps-core
         gnome-shell-extensions zsh zsh-vcs distrobox pipewire
         pipewire-pulse wireplumber networkmanager-wifi
         icu-data-full font-noto font-noto-extra font-noto-emoji
         font-noto-cjk font-noto-symbols apk-polkit-rs"
+
+    local _k _a
+
+    for _k in $kernel_flavors; do
+        apks="$apks linux-$_k"
+        for _a in $kernel_addons; do
+            apks="$apks $_a-$_k"
+        done
+    done
+
+    apks="$apks linux-firmware-none"
 }
